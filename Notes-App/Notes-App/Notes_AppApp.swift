3e7a6bc8ca1765be55c9dbb3ce4039570ec0c3b9
peterkost <46546031+peterkost@users.noreply.github.com>
2021-09-08 07:46:50 +0300

    initial app

---
+++ Notes-App/Notes-App/Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppApp.swift
+//  Notes-App
+//
+//  Created by Peter Kostin on 2021-09-08.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
